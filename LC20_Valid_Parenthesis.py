b553dc4cc58481ad7673b565fe32ea03eb918635
mgar216 <28987804+mgar216@users.noreply.github.com>
2021-01-06 01:17:40 -0500

    Create LC20_Valid_Parenthesis.py

---
+++ LC20_Valid_Parenthesis.py
@@ -0,0 +1,26 @@
+"""
+Runtime: 28 ms, faster than 83.24% of Python3 online submissions for Valid Parentheses.
+Memory Usage: 14.1 MB, less than 95.25% of Python3 online submissions for Valid Parentheses.
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s or len(s) & 1:
+            return False
+        match = {
+            '(':')',
+            '[':']',
+            '{':'}',             
+                }
+        tracking = []
+        last = -1
+        for i in s:
+            if i in match.keys():
+                tracking.append(i)
+            else:
+                if not tracking or match[tracking.pop()] != i:
+                    return False
+        if not tracking:
+            return True
+        else:
+            return False
