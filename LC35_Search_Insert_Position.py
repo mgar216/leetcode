cb55b4171c3d29cbd38b35648cadc580ed9fe9ef
mgar216 <28987804+mgar216@users.noreply.github.com>
2021-01-06 01:38:32 -0500

    Update LC35_Search_Insert_Position.py

--- LC35_Search_Insert_Position.py
+++ LC35_Search_Insert_Position.py
@@ -1,12 +1,8 @@
 """
-Runtime: 48 ms, faster than 71.85% of Python3 online submissions for Search Insert Position.
-Memory Usage: 15 MB, less than 44.17% of Python3 online submissions for Search Insert Position.
+Runtime: 44 ms, faster than 90.68% of Python3 online submissions for Search Insert Position.
+Memory Usage: 15.1 MB, less than 18.16% of Python3 online submissions for Search Insert Position.
 """
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return [n for n, i in enumerate(nums) if i == target][0]
-        else:
-            nums.append(target)
-            return [n for n, i in enumerate(sorted(nums)) if i == target][0]
+        nums.append(target)
+        return [n for n, i in enumerate(sorted(nums)) if i == target][0]
