8fdf6a23e9140965dba37b738c86cebb399052e8
mgar216 <28987804+mgar216@users.noreply.github.com>
2021-01-08 20:51:04 -0500

    Update LC01_TwoSum.py

--- LC01_TwoSum.py
+++ LC01_TwoSum.py
@@ -5,7 +5,7 @@ Memory Usage: 14.3 MB, less than 92.02% of Python3 online submissions for Two Su
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for n, i in enumerate(nums):
-            for p, x in enumerate(nums):
+            for p, x in enumerate(nums[1:], start=1):
                 if (i + x) == target:
                     if n != p:
                         return [n, p]
