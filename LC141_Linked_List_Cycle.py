b9ee19e6652a3fbc3b921ace55ffb9ebc6029fb4
mgar216 <28987804+mgar216@users.noreply.github.com>
2021-02-02 13:34:38 -0500

    Update LC141_Linked_List_Cycle.py

--- LC141_Linked_List_Cycle.py
+++ LC141_Linked_List_Cycle.py
@@ -18,3 +18,29 @@ class Solution:
             location.add(on)
             on = on.next
         return False
+
+"""
+Runtime: 44 ms, faster than 89.89% of Python3 online submissions for Linked List Cycle.
+Memory Usage: 17.2 MB, less than 67.21% of Python3 online submissions for Linked List Cycle.
+Uses Tortoise and Hare pointers (Cycle detection algorithm) to detect cycles—keeps O(1) space complexity.
+https://en.wikipedia.org/wiki/Cycle_detection
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        on = head
+        f = head
+        while f:
+            try:
+                f = f.next.next
+            except AttributeError:
+                return False
+            on = on.next
+            if on == f:
+                return True
+        return False
